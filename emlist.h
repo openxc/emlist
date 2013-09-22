58b3a9ae680dc924610f9b720fa3fdd893a8b976
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-09-22 11:46:40 -0400

    Initial commit.

---
+++ emlist.h
@@ -0,0 +1,15 @@
+#ifndef _EMLIST_H_
+#define _EMLIST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _EMLIST_H_
